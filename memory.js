d9ceaae919256f50072eb77ab079c4fb30411587
Łukasz W <inicjallw@gmail.com>
2021-12-13 20:17:56 +0100

    JS tworzenie tablicy kart w htmlu

--- memory.js
+++ memory.js
@@ -1,3 +1,4 @@
+var ile_kart=0;
 
 window.onload=function(){
     var przycisk = document.getElementById("wczytajGre");
@@ -5,6 +6,37 @@ window.onload=function(){
 };
 
 function getValue(){
-    var ile_kart=document.getElementById("ileKart").value;
-    alert(ile_kart);
+    ile_kart=document.getElementById("ileKart").value;
+  //  alert(ile_kart);
+    
+    rozgrywka();
+};
+
+function rozgrywka(){
+    
+    var zestaw = new Array();
+    var komplet = '';
+ 
+    for (i = 0; i < (ile_kart*2); i++) {
+        zestaw[i]='<div class="card"></div>';
+        komplet=komplet+zestaw[i];
+        //alert(komplet);
+    }
+    
+    switch (ile_kart)
+    {
+        case "4":
+            document.getElementById("board").style.width="600px";
+            break;
+        case "8":
+        case '12':
+        case '16':
+            document.getElementById("board").style.width="1200px";
+            break;
+        default:
+            alert("BŁĄD");
+    }   
+    document.getElementById("board").style.transition=" all .2s ease-in-out";
+    document.getElementById("board").innerHTML = komplet; 
+    
 };
