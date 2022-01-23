9a8a012b8a2a02a19bf220e5de66569ddf02f3e1
Łukasz W <inicjallw@gmail.com>
2022-01-23 21:47:37 +0100

    dzilajaca wersja posiadajaca kilkia bledow

--- memory.js
+++ memory.js
@@ -1,6 +1,5 @@
-var ile_kart=0;
-var przypisKart=new Array();
-var fCardValue;
+var przypisKart = new Array();
+var ile_kart = 0, fCardValue=-1, score = 0 , odkrytoPar=0;
 
 window.onload=function(){
     var przycisk = document.getElementById("wczytajGre");
@@ -10,21 +9,23 @@ window.onload=function(){
 function getValue(){
     ile_kart=document.getElementById("ileKart").value;
   //  alert(ile_kart);
-    
     rozgrywka();
 };
 
 function rozgrywka(){
     
     var zestaw = new Array();
-    var komplet = '';
+    var komplet = '';//Zliczanie tur gracza
+    score = 0;
+    odkrytoPar = 0;
+    $( "#score" ).text( "Wykonanych ruchów: " + score );
  
-    for (i = 0; i < (ile_kart*2); i++) {
+    //STWORZENIE ZAWARTOSCI DIV-A BOARD
+    for (let i = 0; i < (ile_kart*2); i++) {
         zestaw[i]='<div class="card"></div>';
-        komplet=komplet+zestaw[i];
-        //alert(komplet);
+        komplet= komplet + zestaw[i];
     }
-    
+    //OKRESLENIE ROZMIARÓW DIV-A BOARD
     switch (ile_kart)
     {
         case "4":
@@ -45,45 +46,63 @@ function rozgrywka(){
     //Przypisanie wartosci do kart
     addCardValue(ile_kart);
 
-    //****************TU FUNKCJE POKAZANIA CALEJ TABLICY przez 2s 
+    //POKAZANIA CALEJ TABLICY przez 2s 
+    for(let i=0;i<(ile_kart*2);i++){
+        $( ".card:eq( "+ (i) +")" ).css( "background-image" , 'url("img/'+(przypisKart[i])+'.png")' ); 
+        $( ".card:eq( "+ (i) +")" ).toggleClass( 'cardA' );
+    }
 
-    //activating event on click to all elements
-    $( ".card" ).on( "click", function() {
-        var index = $( ".card" ).index( this );
-        var toCheck = 0;
+    //PONOWNE UKRYCIE kART DO GRY
+setTimeout(() => { 
+        //odklejenie klasy cardA od kart tablicy
+        for(let i=0;i<(ile_kart*2);i++){
+            $( ".card:eq( "+ (i) +")" ).toggleClass( 'cardA' );
+        }        
+        document.getElementById("board").innerHTML = komplet; 
+        //activating event on click to all elements
+        $( ".card" ).on( "click", function() {
+            var index = $( ".card" ).index( this );
+            var toCheck = 0;
 
-        //alert( przypisKart[index] );
 
-        //Pokazanie karty
-        showCardImg(index);
-        
-        alert( przypisKart[index] );
-
-
-        //test czy odsloniete juz dwie karty
-        toCheck = $('.cardA').length;
-        if (toCheck >= 2) {
-                  
-            compareCards(fCardValue,przypisKart[index]);
-
-        }else{
-
-            fCardValue=przypisKart[index];
-        }     
-    });
+            if (toCheck = $('.cardA').length<2){
+                //Pokazanie karty
+                showCardImg(index);
+                
+                //test czy odsloniete juz dwie karty
+                toCheck = $('.cardA').length;
+                if (toCheck >= 2) {         
+                    setTimeout(() => { compareCards(fCardValue,przypisKart[index]);
+                        if(odkrytoPar==ile_kart){
+                            //alert("WYGRANA");
+                            document.getElementById("board").innerHTML = "WYGRANA w "+score+" ruchach.";
+                            $( "#score" ).text( "" );
+                        }
+                    }, 700);
+                    //Zliczanie tur gracza
+                    score = score+1;
+                    //Aktualizacja stanu licznika
+                    $( "#score" ).text( "Wykonanych ruchów: "+ score );
+                }
+                else{
+                    fCardValue=przypisKart[index];
+                }
+            }     
+        });
+    }, (ile_kart/2*1000*0.7));//OPUZNIENIE SCHOWANIA TABLICY KART
 };
 
 //Funkcja przypisujaca wartosci do elementów BOARD-a
 function addCardValue(pair_nr){
     
-    for (i=0; i<pair_nr*2; i++){
+    for (let i=0; i<pair_nr*2; i++){
         przypisKart[i]=100;
     }
     //alert (przypisKart.length);
 
     //Przypisz wartosci dla elementów BOARD-a
     var index = 0
-    for (i = 0; i < pair_nr; i++) {
+    for (let i = 0; i < pair_nr; i++) {
         var double = 0
         do { 
             index = getRandomInt(0, przypisKart.length)
@@ -109,39 +128,33 @@ function showCardImg(cardindex){
     $( ".card:eq( "+ (cardindex) +")" ).toggleClass( 'cardA' );
 };
 
-//Funkcja porównania
+//Funkcja porównania KART
 function compareCards(fCard,sCard){
     if (fCard==sCard){
-        alert("Zajebioza: "+sCard+" : "+fCard);
-        for (i=0 ; i<=przypisKart.length;i++){
-        //nie ten wskaznik na element
+        //alert("Zajebioza: "+sCard+" : "+fCard);
+        for (let i=0 ; i<=przypisKart.length;i++){
+        //Wyłączenie widocznosci kart które zostały odgadniete
             if( przypisKart[i]==fCard ){
                 $( ".card:eq( "+ i +")" ).css( "opacity" , '0' );
-                //$( ".card:eq( "+ (sCard) +")" ).css( "opacity" , '0' );    
+                $( ".card:eq( "+ i +")" ).css( "cursor" , 'default' ); 
+                //Wyłączenie EVENTU dla elementu
+                $( ".card:eq( "+ i +")" ).off( );   
             }
         }
-        //odłączenie 
+        //ODKLEJ KLASY DOKLEJONE
         $( ".cardA:eq( "+ (1) +")" ).toggleClass( 'cardA' );
         $( ".cardA:eq( "+ (0) +")" ).toggleClass( 'cardA' );
-
+        odkrytoPar = odkrytoPar + 1;
     } else{
-        alert("LIPA: "+fCard+"#:#"+sCard);
+        //JESLI NIEPOWODZNIE ODKLEJ KLASY DOKLEJONE
         $( ".cardA:eq( "+ (1) +")" ).toggleClass( 'cardA' );
         $( ".cardA:eq( "+ (0) +")" ).toggleClass( 'cardA' );
        
         //zmiana tła karty na domyślne
-        for (i=0 ; i<=przypisKart.length;i++){
-            
+        for (let i=0 ; i<=przypisKart.length;i++){
                 if( (przypisKart[i]==fCard) ||  (przypisKart[i]==sCard) ){
-                 
                     $( ".card:eq( "+ i +")" ).css( "background-image" , 'url("img/karta.png")' );
-                  
                 }
             }
-        
-        
-        
-        
-        
-    }
+     }
 };
